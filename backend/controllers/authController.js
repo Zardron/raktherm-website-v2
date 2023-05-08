84f33b9da3044ad1afdd263b5f0ea043afb7a164
zardron <zardron.pesquera1993@gmail.com>
2023-05-08 15:14:54 +0400

    adding auth function backend

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -7,28 +7,21 @@ const asyncHandler = require("express-async-handler");
 // @route POST /auth
 // @access Public
 const login = asyncHandler(async (req, res) => {
-  const { username, password } = req.body;
+  const { email, password } = req.body;
 
-  if (!username || !password) {
+  if (!email || !password) {
     return res.status(400).json({ message: "All fields are required" });
   }
 
-  const foundUser = await User.findOne({ username }).exec();
+  const foundUser = await User.findOne({ email }).exec();
 
-  if (!foundUser || !foundUser.active) {
-    return res.status(401).json({ message: "Unauthorized" });
-  }
-
-  const match = await bcrypt.compare(password, foundUser.password);
-
-  if (!match)
-    return res.status(401).json({ message: "Invalid username or password!" });
+  if (!foundUser || foundUser.password !== password)
+    return res.status(401).json({ message: "Invalid email or password!" });
 
   const accessToken = jwt.sign(
     {
       UserInfo: {
-        username: foundUser.username,
-        roles: foundUser.roles,
+        email: foundUser.email,
       },
     },
     process.env.ACCESS_TOKEN_SECRET,
@@ -36,7 +29,7 @@ const login = asyncHandler(async (req, res) => {
   );
 
   const refreshToken = jwt.sign(
-    { username: foundUser.username },
+    { email: foundUser.email },
     process.env.REFRESH_TOKEN_SECRET,
     { expiresIn: "7d" }
   );
@@ -49,7 +42,7 @@ const login = asyncHandler(async (req, res) => {
     maxAge: 7 * 24 * 60 * 60 * 1000, //cookie expiry: set to match rT
   });
 
-  // Send accessToken containing username and roles
+  // Send accessToken containing email and roles
   res.json({ accessToken });
 });
 
@@ -70,7 +63,7 @@ const refresh = (req, res) => {
       if (err) return res.status(403).json({ message: "Forbidden" });
 
       const foundUser = await User.findOne({
-        username: decoded.username,
+        email: decoded.email,
       }).exec();
 
       if (!foundUser) return res.status(401).json({ message: "Unauthorized" });
@@ -78,8 +71,7 @@ const refresh = (req, res) => {
       const accessToken = jwt.sign(
         {
           UserInfo: {
-            username: foundUser.username,
-            roles: foundUser.roles,
+            email: foundUser.email,
           },
         },
         process.env.ACCESS_TOKEN_SECRET,
